e2a15cf1f122e152171129e600c36227c9bf7b58
VolAnder <saneexe@yandex.ru>
2018-12-22 21:51:34 +0300

    Add project files.

---
+++ TrainTicketsCheckerBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainTicketsCheckerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
